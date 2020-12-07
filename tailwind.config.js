2642091baf1c14d7bd7e93e692d006708ffaa5b7
Dika Muharman <dika1254@gmail.com>
2020-12-08 05:20:21 +0700

    complite web

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const color = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3182CE",
+        secondary: "#B83280",
+        background: "#FBFAF8",
+      },
+      zIndex: {
+        "-10": "-10",
+        "-20": "-20",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
